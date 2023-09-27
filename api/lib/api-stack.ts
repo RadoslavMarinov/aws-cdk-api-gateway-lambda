2dacc89a6672db00916d64cceff5a13430c444ce
radsolav-marinov <radoslav.k.marinov@gmail.com>
2023-09-27 23:14:54 +0300

    Ready api endpoint with TS lambda

--- api/lib/api-stack.ts
+++ api/lib/api-stack.ts
@@ -1,11 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const api =  new apigateway.RestApi(this, "restapi", {
+      restApiName: "Rent a Car",
+      description: "Rent a Car rest API",
+    })
+
+    api.root.addMethod('GET')
+    const customerResource = api.root.addResource('customers')
+
+    const lambdaEntryPoint = path.join(__dirname, `/lambda-handlers/customers/index.ts`)
+    const lambdaHandler = new NodejsFunction(this, 'my-function', {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'main',
+      entry: lambdaEntryPoint,
+    });
+
+    customerResource.addMethod('GET', new apigateway.LambdaIntegration(lambdaHandler))
     // The code that defines your stack goes here
 
     // example resource
