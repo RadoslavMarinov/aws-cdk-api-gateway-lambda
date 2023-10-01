c80e70254742144d13a15c0b592993b361ded7e3
Radoslav Marinov <radoslav.k.marinov@gmail.com>
2023-10-01 20:15:31 +0300

    Extract api URL from exports

--- api/e2e/customer/GetAllCustomers.e2e.test.ts
+++ api/e2e/customer/GetAllCustomers.e2e.test.ts
@@ -15,20 +15,19 @@ const input = {
 
 describe("Get All customers API endpoint", () => {
   test("Should statusCode 200", async () => {
-    // const getCredentials = fromIni({ profile: "default" });
-    // const credentials = await getCredentials();
-
-
     const credential = await getCredentialsFromCredFiles("default") 
 
     const cfnClient = new CloudFormationClient({
       credentials: credential,
-      region: "eu-west-2",
+      region: process.env.AWS_DEFAULT_REGION || "eu-west-2",
     });
 
     const command = new ListExportsCommand({});
     const response = await cfnClient.send(command);
 
-    expect(response).toBeDefined();
+    const apiUrl = response.Exports?.find(x => x.Name === "apiUrl");
+    expect(apiUrl?.Value).toBeDefined();
+
+    expect(response.$metadata.httpStatusCode).toBe(200);
   });
 });
